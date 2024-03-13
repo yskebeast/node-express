c51a1111e782c30319146a3245c751d0632358ce
yske.biz@gmail.com <yske.biz@gmail.com>
2024-03-14 00:26:31 +0900

    add: first commit

---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+
+const router = express.Router();
+
+// 上のuse()にnext()がないので、以下のuse()は実行されない
+router.get("/", (req, res, next) => {
+  //   console.log("In another middleware");
+  console.log(adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
