94c2bce8670d109d24ac6f294f4416686ee9474c
yske.biz@gmail.com <yske.biz@gmail.com>
2024-03-14 01:13:49 +0900

    add: template using pug file

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// https://expressjs.com/en/4x/api.html#app.set
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -23,6 +27,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,10 @@ const router = express.Router();
 // 上のuse()にnext()がないので、以下のuse()は実行されない
 router.get("/", (req, res, next) => {
   //   console.log("In another middleware");
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
