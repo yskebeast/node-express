d57a639dfa17e2080fdc19211a320d9ef29e6272
yske.biz@gmail.com <yske.biz@gmail.com>
2024-03-15 00:36:42 +0900

    add: pug layout style

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,10 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   //   console.log("In another middleware");
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get("/", (req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
