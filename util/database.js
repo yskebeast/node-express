a3a8a714e16e5a3197ec73147b7217a0d988cb3d
yske.biz@gmail.com <yske.biz@gmail.com>
2024-04-03 23:23:51 +0900

    delete:

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const password = "mXCNfXcm2qwqrzT0";
-const uri = `mongodb+srv://yskebiz:${password}@mongoyus.w7z2ufz.mongodb.net/?retryWrites=true&w=majority&appName=mongoyus`;
+const password = "";
+const uri = ``;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(uri)
