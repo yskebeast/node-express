976948b41f93119500cde1027d2c2dc5d7aaf848
yske.biz@gmail.com <yske.biz@gmail.com>
2024-04-03 23:38:05 +0900

    add: .env file

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const password = "";
-const uri = ``;
+const url = `mongodb+srv://${process.env.PROJECT_NAME}:${process.env.PASSWORD}@mongoyus.w7z2ufz.mongodb.net/?retryWrites=true&w=majority&appName=mongoyus`;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect(uri)
+  MongoClient.connect(url)
     .then((client) => {
       console.log("Connected!");
       callback(client);
