86d9bc0ef0617214b1acf04e3c40cbbb6baf0b4f
yske.biz@gmail.com <yske.biz@gmail.com>
2024-04-03 23:22:40 +0900

    add: connect mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const password = "mXCNfXcm2qwqrzT0";
+const uri = `mongodb+srv://yskebiz:${password}@mongoyus.w7z2ufz.mongodb.net/?retryWrites=true&w=majority&appName=mongoyus`;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
